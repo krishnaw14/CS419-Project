b2f2fd0d473391fa12c63fc61cd1064942c55dfe
krishnaw14 <krishnaw14@gmail.com>
2018-11-26 18:03:06 +0530

    Score display

--- random_action.py
+++ random_action.py
@@ -1,9 +1,12 @@
 import gym
+
 env = gym.make("CartPole-v1")
 observation = env.reset()
+score = 0
 for _ in range(1000):
   env.render()
   action = env.action_space.sample() 
   observation, reward, done, info = env.step(action)
+  score += reward
   if done: 
-  	break
+  	print("Score = ", score)
